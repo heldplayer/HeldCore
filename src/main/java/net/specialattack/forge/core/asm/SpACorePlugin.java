cbf10cea2a623269f4e67567ff331504a3b2defb
Heldplayer <mitchel.pyl@gmail.com>
2014-10-21 22:21:21 +0200

    Remove spam made from missing block and item textures.

--- src/main/java/net/specialattack/forge/core/asm/SpACorePlugin.java
+++ src/main/java/net/specialattack/forge/core/asm/SpACorePlugin.java
@@ -11,7 +11,7 @@ public class SpACorePlugin implements IFMLLoadingPlugin, IFMLCallHook {
 
     @Override
     public String[] getASMTransformerClass() {
-        return new String[] { "net.specialattack.forge.core.asm.SpACoreModTransformer" };
+        return new String[] { "net.specialattack.forge.core.asm.SpACoreModTransformer", "net.specialattack.forge.core.asm.SpACoreLoggerTransformer" };
     }
 
     @Override
